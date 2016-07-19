6213107d75b3a03c5f3e831c8a90efe2c0bc9a7d
Gevik babakhani <gevik@blendjs.com>
2016-07-19 16:42:48 +0200

    savepoint
    Signed-off-by: Gevik babakhani <gevik@blendjs.com>

--- src/buttons.ts
+++ src/buttons.ts
@@ -1,83 +1,84 @@
 var app = new Blend.web.Application({
     mainView: <BoxContainerInterface>{
-        ctype: "lay.hbox",
-        wrap: Blend.eBoxWrap.yes,
-        css: "paper",
-        padding:10,
+        ctype: "lay.flow",
+        padding: 10,
+        defaults: {
+            style: {
+                "margin-bottom": 10
+            }
+        },
         items: [
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button flat no ripple",
-                ripple: false
+            {
+                ctype: "lay.hbox",
+                css: "paper",
+                padding: 10,
+                items: [
+                    <ButtonInterface>{
+                        ctype: "mb.button",
+                        text: "button flat no ripple",
+                        ripple: false
+                    },
+                    {
+                        ctype: "mb.button",
+                        text: "button flat with ripple"
+                    },
+                    <ButtonInterface>{
+                        ctype: "mb.button",
+                        text: "button flat disabled",
+                        disabled: true
+                    }
+                ]
             },
             {
-                ctype: "mb.button",
-                text: "button flat with ripple"
-            },
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button flat disabled",
-                disabled: true
-            },
-            /////////////////////////////////////////////
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button raised no ripple",
-                ripple: false,
-                buttonType: Blend.eButtonType.raised
-            },
-            {
-                ctype: "mb.button",
-                text: "button raised with ripple",
-                buttonType: Blend.eButtonType.raised
-            },
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button raised disabled",
-                disabled: true,
-                buttonType: Blend.eButtonType.raised
-            },
-            /////////////////////////////////////////////
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button flat primary no ripple",
-                ripple: false,
-                buttonType: Blend.eButtonType.flat,
-                theme: "primary"
+                ctype: "lay.hbox",
+                css: "paper",
+                padding: 10,
+                items: [
+                    <ButtonInterface>{
+                        ctype: "mb.button",
+                        text: "button raised no ripple",
+                        ripple: false,
+                        buttonType: Blend.eButtonType.raised
+                    },
+                    {
+                        ctype: "mb.button",
+                        text: "button raised with ripple",
+                        buttonType: Blend.eButtonType.raised
+                    },
+                    <ButtonInterface>{
+                        ctype: "mb.button",
+                        text: "button raised disabled",
+                        disabled: true,
+                        buttonType: Blend.eButtonType.raised
+                    }
+                ]
             },
             {
-                ctype: "mb.button",
-                text: "button flat primary with ripple",
-                buttonType: Blend.eButtonType.flat,
-                theme: "primary"
-            },
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button flat primary disabled",
-                disabled: true,
-                buttonType: Blend.eButtonType.flat,
-                theme: "primary"
-            },
-            /////////////////////////////////////////////
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button raised primary no ripple",
-                ripple: false,
-                buttonType: Blend.eButtonType.raised,
-                theme: "primary"
-            },
-            {
-                ctype: "mb.button",
-                text: "button raised primary with ripple",
-                buttonType: Blend.eButtonType.raised,
-                theme: "primary"
-            },
-            <ButtonInterface>{
-                ctype: "mb.button",
-                text: "button raised primary disabled",
-                disabled: true,
-                buttonType: Blend.eButtonType.raised,
-                theme: "primary"
+                ctype: "lay.hbox",
+                css: "paper",
+                padding: 10,
+                items: [
+                    <ButtonInterface>{
+                        ctype: "mb.button",
+                        text: "button flat primary no ripple",
+                        ripple: false,
+                        buttonType: Blend.eButtonType.flat,
+                        theme: "primary"
+                    },
+                    {
+                        ctype: "mb.button",
+                        text: "button flat primary with ripple",
+                        buttonType: Blend.eButtonType.flat,
+                        theme: "primary"
+                    },
+                    <ButtonInterface>{
+                        ctype: "mb.button",
+                        text: "button flat primary disabled",
+                        disabled: true,
+                        buttonType: Blend.eButtonType.flat,
+                        theme: "primary"
+                    }
+                ]
             }
 
         ]
